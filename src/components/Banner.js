0e9ffc83b17b26c8f641c086db85bc0fde1d8de1
Davyd Kuleba <kulebadavid@gmail.com>
2023-12-29 22:09:57 -0500

    fix typing text error

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,52 +1,53 @@
 import { useState, useEffect } from "react";
-import { Container, Row, Col} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/img/Banner-img.png"
+import headerImg from "../assets/img/Banner-img.png";
 
-export const Banner = () =>{
-
+export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Front-End Developer", "iOS Engineer", "Computer Science Student", "Amateur Climber", "Professional Ramen Eater"];
-    const [text, setText] = useState('')
-    const [delta, setDelta] = useState(300 - Math.random() * 100); 
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000;
 
     useEffect(() => {
+
+        const toRotate = ["Front-End Developer", "iOS Engineer", "Computer Science Student", "Amateur Climber", "Professional Ramen Eater"];
+
+        const tick = () => {
+            let i = loopNum % toRotate.length;
+            let fullText = toRotate[i];
+            let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+            setText(updatedText);
+
+            if (isDeleting) {
+                setDelta(prevDelta => prevDelta / 2);
+            }
+
+            if (!isDeleting && updatedText === fullText) {
+                setIsDeleting(true);
+                setDelta(period);
+            } else if (isDeleting && updatedText === '') {
+                setIsDeleting(false);
+                setLoopNum(loopNum + 1);
+                setDelta(500);
+            }
+        };
+
         let ticker = setInterval(() => {
             tick();
-        }, delta)
+        }, delta);
 
-        return () => { clearInterval(ticker)};
-    }, [text])
-
-    const tick = () =>{
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let UpdatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-        setText(UpdatedText);
-
-        if(isDeleting){
-            setDelta(prevDelta => prevDelta /2)
-        }
-
-        if(!isDeleting && UpdatedText === fullText){
-            setIsDeleting(true);
-            setDelta(period);
-        } else if(isDeleting && UpdatedText === ''){
-            setIsDeleting(false);
-            setLoopNum(loopNum + 1);
-            setDelta(500);
-        }
-    }
+        return () => { clearInterval(ticker) };
+    }, [delta, text, isDeleting, loopNum]);
 
     const scrollToContacts = () => {
         const contactsElement = document.getElementById('connect');
         if (contactsElement) {
-          contactsElement.scrollIntoView({ behavior: 'smooth' });
+            contactsElement.scrollIntoView({ behavior: 'smooth' });
         }
-      };
+    };
 
 
     return (
