7ba100d1fb76d2a038be454b00fb6a28ff372bf4
Davyd Kuleba <kulebadavid@gmail.com>
2023-12-31 00:34:09 -0500

    fix netlify form/react

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,17 @@ import contactImg from "../assets/img/contact-img1.png"
 
 export const ContactForm = () =>{
     return(
+
         <section className="contact" id="connect">
+
+        <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+            <input type="text" name="firstname" />
+            <input type="text" name="lastname" />
+            <input type="email" name="email" />
+            <input type="tel" name="phone" />
+            <textarea name="message"></textarea>
+        </form>
+
             <Container>
                 <Row>
                     <Col lg={6} className="contact-image-wrapper">
@@ -13,7 +23,7 @@ export const ContactForm = () =>{
                     </Col>
                     <Col lg={6}>
                         <h2>Get In Touch</h2>
-                        <form name="contact" method="POST" data-netlify="true">
+                        <form name="contact" method="post">
                             <Row>
                                 <Col sm={6} className="px-1">
                                     <input type="text" name="firstname" placeholder="First Name" />
