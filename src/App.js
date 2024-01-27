5b71d97b3b91c2c26cf49b0f7781cba372387fdd
glauuucoma <kulebadavid@gmail.com>
2024-01-27 13:51:17 -0500

    chat page update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
-import React from 'react';
-import { NavBar } from './components/NavBar';
-import { Banner } from './components/Banner';
-import { Skills } from './components/Skills'
-import { Projects } from './components/Projects'
-import { ContactForm } from './components/Contact'
-import { Footer } from './components/Footer';
-import './bootstrap.min.css'
+import "./App.css";
+import React from "react";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { ContactForm } from "./components/Contact";
+import { Footer } from "./components/Footer";
+import "./bootstrap.min.css";
 
 function App() {
   return (
-    <div className='App'>
-        <NavBar />
-        <Banner />
-        <Skills />
-        <Projects />
-        <ContactForm/>
-        <Footer />
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <ContactForm />
+      <Footer />
     </div>
   );
-} 
+}
 
 export default App;
